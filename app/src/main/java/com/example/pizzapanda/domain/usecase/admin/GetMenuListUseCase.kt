2b7d4b02ed8f8bd381bdf28e2ce02c7307c1a8c7
Nyan-Thar-Myint <nyanthar.ktu.it@gmail.com>
2022-09-27 09:05:11 +0630

    Fix: getting menu list

--- app/src/main/java/com/example/pizzapanda/domain/usecase/admin/GetMenuListUseCase.kt
+++ app/src/main/java/com/example/pizzapanda/domain/usecase/admin/GetMenuListUseCase.kt
@@ -1,9 +1,10 @@
 package com.example.pizzapanda.domain.usecase.admin
 
+import com.example.pizzapanda.domain.model.Menu
 import com.example.pizzapanda.domain.repository.MenuRepository
 
 class GetMenuListUseCase(private val menuRepository: MenuRepository) {
-    suspend operator fun invoke() {
-        menuRepository.getMenus()
+    suspend operator fun invoke(): List<Menu> {
+        return menuRepository.getMenus()
     }
 }
